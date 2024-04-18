42ae9b20640888e1f3afed871e2842a0010d23da
qruell <illya.goncharuk.angelok@gmail.com>
2024-04-18 21:14:26 +0300

    hm ex2

--- main.cpp
+++ main.cpp
@@ -4,25 +4,25 @@
 int main() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED); // Червоний колір для слова "Every"
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED); 
     std::cout << "Every" << "\n";
 
-    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN); // Зелений колір для слова "Hunter"
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN); 
     std::cout << "\tHunter" << "\n";
 
-    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE); // Синій колір для слова "Wants"
+    SetConsoleTextAttribute(hConsole, FOREGROUND_BLUE); 
     std::cout << "\t\tWants" << "\n";
 
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN); // Жовто-зелений колір для слова "To know"
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN); 
     std::cout << "\t\t\tTo know" << "\n";
 
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE); // Фіолетовий колір для слова "Where"
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_BLUE); 
     std::cout << "\t\t\t\t Where" << "\n";
 
-    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE); // Бірюзовий колір для слова "Does Pheasant"
+    SetConsoleTextAttribute(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE); 
     std::cout << "\t\t\t\t\tDoes Pheasant" << "\n";
 
-    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Білий колір для слова "Sit"
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); 
     std::cout << "\t\t\t\t\t\t\tSit" << "\n\n";
 
     return 0;
