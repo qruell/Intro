8f810166324bdd6e773e2f2ef7907acb6db3ba7d
qruell <illya.goncharuk.angelok@gmail.com>
2024-04-18 21:07:41 +0300

    hm ex1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    std::cout << "\"Мені однаково, чи буду…\" Тараса Шевченка:\n"
+        "Мені однаково, чи буду\n"
+        "Я  жить в Україні, чи ні.\n"
+        "Чи хто згадає, чи забуде\n"
+        "Мене в снігу на чужині —\n";
+        "Однаковісінько мені.";
+    return 0;
+}
