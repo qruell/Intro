fe07078fd41fdb82564a230303a8aad57317063c
qruell <illya.goncharuk.angelok@gmail.com>
2024-04-18 20:51:39 +0300

    lab ex3

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
+#include <windows.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "\\\\Dennis Ritchie\\\\" << std::endl;
-
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    std::cout << "To be, ";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED);
+    std::cout << "or not ";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+    std::cout << "to be: that is the question:\n";
+    SetConsoleTextAttribute(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+    std::cout << "'tis nobler in the mind to suffer\n"
+        "The slings and arrows of outrageous fortune,\n"
+        "Or to take arms against a sea of troubles,\n"
+        "And by opposing end them?\n"
+        "William Shakespeare";
     return 0;
 }
